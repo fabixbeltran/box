7ccc0451cc416c888d5073e2b890aa6addd1afcd
Fabian Xavier Beltran <60524414+fabixbeltran@users.noreply.github.com>
2022-05-23 13:59:57 -0300

    Add files via upload

--- box.js
+++ box.js
@@ -22,3 +22,14 @@ Sortable.create(boxesReady,{
     },
     animation: 300,
 });
+
+
+
+
+const accordion = document.getElementsByClassName('container');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
